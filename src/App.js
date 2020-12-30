11a1ab10c4ca71b82960dcf8937e58d4ce8727be
Peter Sekesan <psekesan@gmail.com>
2020-12-30 14:29:03 -0500

    initial google api assignment commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export  class App extends Component {
+  
+  state ={
+    books: [],
+    query: 'shining'
+}
+  
+  searchBooks = () => {
+    fetch(`https://www.googleapis.com/books/v1/volumes?q={this.state.query}`)
+    .then(res => res.json())
+    .then((res)=> this.setState({books:res.items}))
+}
+ 
+componentDidMount () {
+    this.searchBooks();
+
+  }
+
+
+
+render() {
+    return (
+      <div className="App">
+        <h1> Pete's Book Search</h1>
+        <form>
+          <input type="text" value={this.state.query} placeholder= "Book Search" aria-label="Book Search Query"/>
+        </form>
+        <p>Your search for "{this.state.query}"</p>
+        <p> We found {this.state.books.length} results</p>
+        <ul>
+          {this.state.books.map(book =><li>{book.volumeInfo.title}</li>)}
+        </ul>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
