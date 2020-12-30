ae39651ec2eccfe5bdefb4d12e92df14209e1a93
Peter Sekesan <psekesan@gmail.com>
2020-12-30 14:50:28 -0500

    calling api

--- src/App.js
+++ src/App.js
@@ -1,39 +1,48 @@
 import React, { Component } from 'react'
 
 export  class App extends Component {
-  
-  state ={
+  state = {
     books: [],
-    query: 'shining'
-}
-  
+    query: "shining",
+  };
+
   searchBooks = () => {
     fetch(`https://www.googleapis.com/books/v1/volumes?q={this.state.query}`)
-    .then(res => res.json())
-    .then((res)=> this.setState({books:res.items}))
-}
- 
-componentDidMount () {
+      .then((res) => res.json())
+      .then((res) => this.setState({ books: res.items }));
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
     this.searchBooks();
+  };
 
+  componentDidMount() {
+    this.searchBooks();
   }
 
-
-
-render() {
+  render() {
     return (
       <div className="App">
         <h1> Pete's Book Search</h1>
-        <form>
-          <input type="text" value={this.state.query} placeholder= "Book Search" aria-label="Book Search Query"/>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            value={this.state.query}
+            placeholder="Book Search"
+            aria-label="Book Search Query"
+            onChange={(e) => this.setState({ query: e.target.value })}
+          />
+          <input type="submit" value="Go" />
         </form>
         <p>Your search for "{this.state.query}"</p>
         <p> We found {this.state.books.length} results</p>
         <ul>
-          {this.state.books.map(book =><li>{book.volumeInfo.title}</li>)}
+          {this.state.books.map((book) => (
+            <li>{book.volumeInfo.title}</li>
+          ))}
         </ul>
       </div>
-    )
+    );
   }
 }
 
